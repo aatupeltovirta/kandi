37df5a18caee1433d47bf9c9bdab7670c9015940
Aatu Peltovirta <aatupeltovirta@gmail.com>
2025-02-13 15:25:45 +0200

    käytetään B/A sidosenergian tilalla

--- gaussianprocess2016.py
+++ gaussianprocess2016.py
@@ -6,7 +6,7 @@ import statistics
 import matplotlib.pyplot as plt
 
 # Read the file Windows
-data = np.genfromtxt(r'C:\Users\atubb\kandi\EXP2016.dat', delimiter=',', skip_header=1, usecols=(4, 3, 7))
+data = np.genfromtxt(r'C:\Users\atubb\kandi\EXP2016.dat', delimiter=',', skip_header=1, usecols=(4, 3, 7, 9))
 
 # Read the file Ubuntu
 #data = np.genfromtxt(r'/home/aajamape/kandi/EXP2016.dat', delimiter=',', skip_header=1, usecols=(4, 3, 7))
@@ -16,6 +16,7 @@ data = np.genfromtxt(r'C:\Users\atubb\kandi\EXP2016.dat', delimiter=',', skip_he
 Z = data[:, 0]  # Column 2
 N = data[:, 1]  # Column 3
 Binding_Energy = data[:, 2]  # Column 7
+BperA = data[:, 3]
 
 # Create a Gaussian process model
 kernel = RBF(1, (1, 1e3))
@@ -24,8 +25,8 @@ model = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10)
 # Train the model
 Z_and_N = np.column_stack((Z, N))
 
-X_train, X_val, y_train, y_val = train_test_split(Z_and_N, Binding_Energy, test_size=0.3, random_state=42)
-y = Binding_Energy
+X_train, X_val, y_train, y_val = train_test_split(Z_and_N, BperA, test_size=0.3, random_state=42)
+
 model.fit(X_train, y_train)
 
 # Use the model to predict binding energies for new data
@@ -44,7 +45,7 @@ average = statistics.mean(binding_energy_diff)
 
 # Create a scatter plot with a color map
 plt.figure(figsize=(10, 6))
-sc = plt.scatter(X_val[:,0],X_val[:,1], c=binding_energy_diff, cmap='viridis', s=50)
+sc = plt.scatter(X_val[:,0],X_val[:,1], c=binding_energy_diff, cmap='seismic', s=50)
 plt.colorbar(sc, label='Ennustettu sidosenergia - Mitattu sidosenergia')
 plt.xlabel('Neutronien määrä N')
 plt.ylabel('Protonien määrä Z')
--- testi1.py
+++ testi1.py
@@ -6,7 +6,7 @@ import statistics
 import matplotlib.pyplot as plt
 
 # Read the file
-data = np.genfromtxt(r'C:\Users\atubb\kandi\EXP2016.dat', delimiter=',', skip_header=1, usecols=(4, 3, 7, 2))
+data = np.genfromtxt(r'C:\Users\atubb\kandi\EXP2016.dat', delimiter=',', skip_header=1, usecols=(4, 3, 7, 2, 9))
 
 #data = np.genfromtxt(r'/home/aajamape/kandi/EXP2016.dat', delimiter=',', skip_header=1, usecols=(4, 3, 7))
 
@@ -20,6 +20,10 @@ Binding_Energy = data[:, 2]  # Column 7
 
 A = data[:, 3] # Column 1
 
+BperAA = data[:, 4]
+
+
+
 #Binding energy / mass number
 BperA = Binding_Energy / A
 
@@ -29,7 +33,7 @@ model = MLPRegressor(hidden_layer_sizes=(100, 50), max_iter=10000, solver='adam'
 # Train the model
 Z_and_N = np.column_stack((Z, N))
 
-X_train, X_val, y_train, y_val = train_test_split(Z_and_N, BperA, test_size=0.3, random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(Z_and_N, BperAA, test_size=0.3, random_state=42)
 
 y = Binding_Energy
 model.fit(X_train, y_train)
@@ -38,15 +42,18 @@ model.fit(X_train, y_train)
 
 predicted_be = model.predict(X_val)
 
-#Mean squared error
 
-mse = mean_squared_error(y_val, predicted_be)
 
 # Convert B/A back to only binding energy
 AA = X_val[:,0] + X_val[:,1]
 
-final_binding_energy = y_val * AA
-final_predicted_be = predicted_be * AA
+final_binding_energy = y_val 
+final_predicted_be = predicted_be
+
+
+#Mean squared error
+
+mse = mean_squared_error(final_binding_energy, final_predicted_be)
 
 
 #Calculate difference of new and known binding energies
@@ -57,7 +64,7 @@ average = statistics.mean(binding_energy_diff)
 
 # Create a scatter plot with a color map
 plt.figure(figsize=(10, 6))
-sc = plt.scatter(X_val[:,0],X_val[:,1], c=binding_energy_diff, cmap='viridis', s=50)
+sc = plt.scatter(X_val[:,0],X_val[:,1], c=binding_energy_diff, cmap='seismic', s=50)
 plt.colorbar(sc, label='Ennustettu sidosenergia - Mitattu sidosenergia')
 plt.xlabel('Neutronien määrä N')
 plt.ylabel('Protonien määrä Z')
